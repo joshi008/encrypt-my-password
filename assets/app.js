b82890cb67b1159e99b65f94240286dee4be417d
Hrishabh Joshi <joshi008@users.noreply.github.com>
2021-01-14 20:04:08 +0530

    password encryption

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,18 @@
 function encrypt() {
-    let password = document.getElementById("password")
-    let key = document.getElementById("key1")
+    let password = document.getElementById("password").value
+    let key = document.getElementById("key1").value
+    console.log(password + "  " + key)
 
     let encrypted = CryptoJS.AES.encrypt(password, key);
     document.getElementById("result1").innerHTML = encrypted;
+}
+
+function decrypt() {
+    let password = document.getElementById("password2").value
+    let key = document.getElementById("key2").value
+
+    let decrypted = CryptoJS.AES.decrypt(password, key);
+    let de = decrypted.toString(CryptoJS.enc.Utf8);
+    console.log("Decrypted = " + de + "  " + password + "   " + key)
+    document.getElementById("result2").innerHTML = de;
 }
